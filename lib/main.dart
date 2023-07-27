fbf1bed893087423b744b8847ce74d963744e1a5
OussemaZaier <medoussemazaier@gmail.com>
2023-07-27 13:59:38 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:whatsapp_clone/screens/splash_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_ANON_KEY'),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/home_page.dart';
+import 'package:whatsapp_clone/screens/register_page.dart';
+import 'package:whatsapp_clone/utils/constants.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    await Future.delayed(Duration.zero);
+    final session = supabase.auth.currentSession;
+    if (session == null) {
+      Navigator.of(context)
+          .pushAndRemoveUntil(RegisterPage.route(), (route) => false);
+    } else {
+      Navigator.of(context)
+          .pushAndRemoveUntil(HomePage.route(), (route) => false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: preloader,
+    );
+  }
+}
